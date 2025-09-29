02fa248b2e04e0db2fa6aac87282df175aec3f7a
Chupapi0 <cristianjaviergayosso@gmail.com>
2025-09-29 13:59:57 +0000

    LISTO numero 6

--- 1_parcial/6_ejercicio/ejercicio_6.py
+++ 1_parcial/6_ejercicio/ejercicio_6.py
@@ -6,18 +6,43 @@ Diseñe un programa en python que realize lo siguiente
 """
 import statistics
 
-while True:
-    pe = int(input("Ingrese el numero de personas en su grupo: "))
-    print(pe)
+pe = int(input("Ingrese el numero de personas en su grupo: "))
+print(pe)
 
-    personas = []
-    edad = []
+personas = []
+edad = []
 
-    for X in range(pe):
-        nombres = input(f"Ingresa el nombre de la persona numero [{X + 1}]: ")
-        personas.append(nombres)
+for X in range(pe):
+    nombres = input(f"Ingresa el nombre de la persona numero [{X + 1}]: ")
+    personas.append(nombres)
+    valor = int(input(f"Ingresa la edad de {nombres}: "))
+    edad.append(valor)
+
+print("Arreglo de personas:", personas)
+print("Arreglo de edades:", edad)
+
+edad_min = min(edad)
+edad_max = max(edad)
+promedio = sum(edad) / len(edad)
+mediana = statistics.median(edad)
+min_index = edad.index(edad_min)
+max_index = edad.index(edad_max)
+print(f"\nLa edad minima es {edad_min} de {personas[min_index]}")
+print(f"La edad maxima es {edad_max} de {personas[max_index]}")
+print(f"El promedio de las edades es: {promedio:.2f}")
+print(f"La mediana de las edades es: {mediana}")
+
+num = int(input("Quieres introducir a otra persona (0 = NO, 1 = YES): "))
+if num == 0:
+   print("Gracias por usar el programa")
+else:
+    np = int(input("¿Cuantas nuevas personas vas a introducir?"))
+    X = len(edad)
+    for X in range(np):
+        nombres = input(f"Ingresa el nombre de la nueva persona numero [{X + 1}]: ")
+        personas = personas + [nombres]
         valor = int(input(f"Ingresa la edad de {nombres}: "))
-        edad.append(valor)
+        edad = edad + [valor]
 
     print("Arreglo de personas:", personas)
     print("Arreglo de edades:", edad)
@@ -32,10 +57,5 @@ while True:
     print(f"La edad maxima es {edad_max} de {personas[max_index]}")
     print(f"El promedio de las edades es: {promedio:.2f}")
     print(f"La mediana de las edades es: {mediana}")
-
-    num = int(input("Enter a number (0 to stop): "))
-
-    if num == 0:
-       break
 
 input("Presione enter para salir.....")
